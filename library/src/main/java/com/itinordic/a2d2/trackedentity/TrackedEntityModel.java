cc5552f27106bff53b605e626742c3335d3a686e
regnatpopulus <matavirer@gmail.com>
2018-11-04 00:19:51 +0100

    starting tracked entity instances

---
+++ library/src/main/java/com/itinordic/a2d2/trackedentity/TrackedEntityModel.java
@@ -0,0 +1,14 @@
+package com.itinordic.a2d2.trackedentity;
+
+import androidx.room.Entity;
+
+/**
+ * Created by regnatpopulus on 03/11/2018.
+ * dev@itinordic.com
+ * Copyright (c) 2018, ITINordic
+ * All rights reserved.
+ */
+
+@Entity
+public class TrackedEntityModel {
+}
