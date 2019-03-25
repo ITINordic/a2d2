07e18bc10aa739c652b6519a18a3ceebbcb66764
Adrian mutize <a.mutize.am@gmail.com>
2019-03-25 11:17:05 +0200

    added tracker associate value type

---
+++ library/src/main/java/com/itinordic/a2d2/trackedentityinstanceattributevalue/TrackedEntityInstanceTrackerAssociateValueDao.java
@@ -0,0 +1,32 @@
+package com.itinordic.a2d2.trackedentityinstanceattributevalue;
+
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+import io.reactivex.Flowable;
+
+@Dao
+public interface TrackedEntityInstanceTrackerAssociateValueDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+    void insert(TrackedEntityInstanceTrackerAssociateValueModel trackedEntityInstanceTrackerAssociateValueModel);
+
+    @Update
+    void update(TrackedEntityInstanceTrackerAssociateValueModel trackedEntityInstanceTrackerAssociateValueModel);
+
+    @Query("SELECT * FROM TrackedEntityInstanceTrackerAssociateValueModel where trackedEntityTypeId = :trackedEntityTypeId AND trackedEntityAttributeId = :trackedEntityAttributeId AND trackedEntityInstanceId = :trackedEntityInstanceId LIMIT 1")
+    Flowable<List<TrackedEntityInstanceTrackerAssociateValueModel>> getTrackedEntityInstanceTrackerAssociateValueModel(String trackedEntityTypeId,
+                                                                                        String trackedEntityAttributeId,
+                                                                                        String trackedEntityInstanceId);
+
+    @Query("SELECT * FROM TrackedEntityInstanceTrackerAssociateValueModel where trackedEntityTypeId = :trackedEntityTypeId AND trackedEntityAttributeId = :trackedEntityAttributeId AND trackedEntityInstanceId = :trackedEntityInstanceId LIMIT 1")
+    List<TrackedEntityInstanceTrackerAssociateValueModel> syncGetTrackedEntityInstanceTrackerAssociateValueModel(String trackedEntityTypeId,
+                                                                                  String trackedEntityAttributeId,
+                                                                                  String trackedEntityInstanceId);
+
+}
